f43e9ed4c8a783161d94b43b69f185d1b925993e
Patryk <patryk.gielcz@gmail.com>
2022-03-15 14:18:13 +0100

    test method for 102

--- TestSampleProgram/ProgramUnitTest.cs
+++ TestSampleProgram/ProgramUnitTest.cs
@@ -1,10 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SampleProgram;
 
 namespace TestSampleProgram
 {
     [TestClass]
     public class ProgramUnitTest
     {
-
+        [TestMethod]
+        public void TestIsEven102()
+        {
+            SampleProgram.Program program = new Program();
+            bool testedValue = program.isEven(102);
+            Assert.IsTrue(testedValue);
+        }
     }
 }
