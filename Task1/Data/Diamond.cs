ae6550dad08cc1d2d0af3a4a201b0c0cc4aa1e39
Patryk <patryk.gielcz@gmail.com>
2022-04-03 14:26:37 +0200

    diamond class

---
+++ Task1/Data/Diamond.cs
@@ -0,0 +1,19 @@
+﻿namespace Data
+{
+    enum QualityValue { Good, VeryGood, Excellent };
+
+    internal class Diamond
+    {
+        private float Mass { get; set; }
+        private float Price { get; set; }
+        private QualityValue Quality { get; set; }
+
+        internal Diamond(float mass, float price, QualityValue quality)
+        {
+            Mass = mass;
+            Price = price;
+            Quality = quality;
+        }
+
+    }
+}
