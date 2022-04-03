c7a84ae8b0e628fcd92145544ae95fdf2df20c35
Patryk <patryk.gielcz@gmail.com>
2022-04-03 13:04:25 +0200

    customer class

---
+++ Task1/Data/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace Data
+{
+    public class Customer
+    {
+        private int Id { get; set; }
+        private string Name { get; set; }
+
+        public Customer(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
