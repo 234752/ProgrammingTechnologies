a115e9c5fa595a02b817f541e62604b2748a3843
Patryk <patryk.gielcz@gmail.com>
2022-04-07 20:24:19 +0200

    event abstraction

--- Task1/Data/Event.cs
+++ Task1/Data/Event.cs
@@ -1,23 +1,8 @@
 namespace Data
 {
-    internal class Event //this class must be abstract
+    internal abstract class Event //this class must be abstract
     {
-        private string Date;
-        private List<Diamond> RemovedFromStorage = new List<Diamond>();
-        private List<Diamond> AddedToStorage = new List<Diamond>();
-        
-        internal Event(string date)
-        {
-            this.Date = date;
-        }
-
-        internal void NoteRemovedDiamond(Diamond diamond)
-        {
-            RemovedFromStorage.Add(diamond);
-        }
-        internal void NoteAddedDiamond(Diamond diamond)
-        {
-            AddedToStorage.Add(diamond);
-        }
+        protected string Date;
+        protected StorageEntry Entry;
     }
 }
