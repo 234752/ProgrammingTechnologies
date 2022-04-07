4ec3c595abd95c789340138f1eeedc07f8f0249a
Patryk <patryk.gielcz@gmail.com>
2022-04-07 19:39:30 +0200

    new class with all the collections

---
+++ Task1/Data/DataContext.cs
@@ -0,0 +1,10 @@
+﻿namespace Data
+{
+    internal class DataContext
+    {
+        private List<Customer> _customers;
+        private List<StorageState> _storageState; //diamonds in storage; each object represents single diamond and has to reference a catalog entry
+        private List<Event> _events;    //events that change storageState
+        private Dictionary<int, Diamond> _catalog;  //list of unique diamonds, will be referenced in events and storage
+    }
+}
