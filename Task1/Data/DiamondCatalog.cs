d04ad3a21e25a6a51023a09c2b90066539d45475
olabanasiak <234750@edu.p.lodz.pl>
2022-04-07 14:12:21 +0200

    updated catalog

--- Task1/Data/DiamondCatalog.cs
+++ Task1/Data/DiamondCatalog.cs
@@ -1,23 +1,15 @@
-﻿using System.Collections.Generic;
-
-namespace Data
-{
-  	class DiamondCatalog : Dictionary<int, Diamond>
+﻿internal class DiamondCatalog : Dictionary<int, Diamond>
     {
-        Dictionary <int, Diamond> DiamondList = new Dictionary <int, Diamond>();
+      
        
         Diamond first = new Diamond(1F, 2999.99F, QualityValue.Good, ShapeValue.Oval);
-        Diamond second = new Diamond(1F, 3999.99F, QualityValue.Good, ShapeValue.Round);
-        Diamond third = new Diamond(2F, 4999.99F, QualityValue.VeryGood, ShapeValue.Pear);
-        Diamond fourth = new Diamond(2F, 6999.99F, QualityValue.Excellent, ShapeValue.Cushion);
-        Diamond fith = new Diamond(3F, 1999.99F, QualityValue.Good, ShapeValue.Oval);
-        Diamond sixth = new Diamond(3F, 5999.99F, QualityValue.Excellent, ShapeValue.Pear);
-        DiamondList.Add(1,first);
-        DiamondList.Add(2,second);
-        DiamondList.Add(3,third);
-        DiamondList.Add(4,fourth);
-        DiamondList.Add(5,fith);
-        DiamondList.Add(6,sixth);
+
+       DiamondCatalog(Dictionary<int, Diamond> diamond) { 
+
+        diamond.Add(1,first);
+            
+        }
+
 
             public override bool Equals(object obj)
         {
@@ -26,17 +18,19 @@ namespace Data
                 return false;
             }
 
-            if (!obj is Diamond)
+            if (obj is Diamond)
             {
                 return false;
             }
           //to do  return ....Comparer.Equals ??
+		  return true; //not correct
         }
 
-        override int GetHashCode()
+       public override int GetHashCode()
         {
             return base.GetHashCode(); //return Comparer......
         }
-    }
 
+
+    }
 }
---
+++ Task1/Data/StorageState.cs
@@ -0,0 +1,11 @@
+namespace Data
+{
+    internal class StorageState : ICollection<DiamondCatalog> //idk if this is correct here
+    {
+        List<Order> OrderList = new List<Order>();
+
+
+ 
+    }
+    }
+}
