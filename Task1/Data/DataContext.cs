b9927d02a123b71b4f6e285f4a7d638aa7d2096a
Patryk <patryk.gielcz@gmail.com>
2022-04-07 19:50:25 +0200

    dataContext implemented?

--- Task1/Data/DataContext.cs
+++ Task1/Data/DataContext.cs
@@ -6,5 +6,28 @@
         private List<StorageState> _storageState; //diamonds in storage; each object represents single diamond and has to reference a catalog entry
         private List<Event> _events;    //events that change storageState
         private Dictionary<int, Diamond> _catalog;  //list of unique diamonds, will be referenced in events and storage
+
+        internal DataContext()
+        {
+            _customers = new List<Customer>();
+            _storageState = new List<StorageState>();
+            _events = new List<Event>();
+            _catalog = new Dictionary<int, Diamond>();
+        }
+        internal void InitializeCatalog()
+        {
+            Diamond first = new Diamond(1F, 2999.99F, QualityValue.Good, ShapeValue.Oval);
+            Diamond second = new Diamond(1F, 3999.99F, QualityValue.Good, ShapeValue.Round);
+            Diamond third = new Diamond(2F, 4999.99F, QualityValue.VeryGood, ShapeValue.Pear);
+            Diamond fourth = new Diamond(2F, 6999.99F, QualityValue.Excellent, ShapeValue.Cushion);
+            Diamond fith = new Diamond(3F, 1999.99F, QualityValue.Good, ShapeValue.Oval);
+            Diamond sixth = new Diamond(3F, 5999.99F, QualityValue.Excellent, ShapeValue.Pear);
+            _catalog.Add(1, first);
+            _catalog.Add(2, second);
+            _catalog.Add(3, third);
+            _catalog.Add(4, fourth);
+            _catalog.Add(5, fith);
+            _catalog.Add(6, sixth);
+        }
     }
 }
