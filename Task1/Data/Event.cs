1fb4bc183a1393af0cb4b0a45a45b1b449fa0d4d
Patryk <patryk.gielcz@gmail.com>
2022-04-07 11:50:34 +0200

    event implemented

--- Task1/Data/Event.cs
+++ Task1/Data/Event.cs
@@ -2,6 +2,22 @@ namespace Data
 {
     internal class Event
     {
+        private string date;
+        private List<Diamond> removedFromStorage = new List<Diamond>();
+        private List<Diamond> addedToStorage = new List<Diamond>();
 
+        public Event(string date)
+        {
+            this.date = date;
+        }
+
+        public void NoteRemovedDiamond(Diamond diamond)
+        {
+            removedFromStorage.Add(diamond);
+        }
+        public void NoteAddedDiamond(Diamond diamond)
+        {
+            addedToStorage.Add(diamond);
+        }
     }
 }
