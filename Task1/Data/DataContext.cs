a253f05ce819236d3efa2d0b4e66364734e0f88d
Patryk <patryk.gielcz@gmail.com>
2022-04-07 20:13:05 +0200

    StorageEntry implemented

--- Task1/Data/DataContext.cs
+++ Task1/Data/DataContext.cs
@@ -3,14 +3,14 @@
     internal class DataContext
     {
         private List<Customer> _customers;
-        private List<StorageState> _storageState; //diamonds in storage; each object represents single diamond and has to reference a catalog entry
+        private List<StorageEntry> _storageState; //diamonds in storage; each object represents single diamond and has to reference a catalog entry
         private List<Event> _events;    //events that change storageState
         private Dictionary<int, Diamond> _catalog;  //list of unique diamonds, will be referenced in events and storage
 
         internal DataContext()
         {
             _customers = new List<Customer>();
-            _storageState = new List<StorageState>();
+            _storageState = new List<StorageEntry>();
             _events = new List<Event>();
             _catalog = new Dictionary<int, Diamond>();
         }
--- Task1/Data/StorageEntry.cs
+++ Task1/Data/StorageEntry.cs
@@ -1,6 +1,12 @@
 ﻿namespace Data
 {
     internal class StorageEntry
-    {
+    { //represents single diamond that is in the storage and avaiable for customers
+        private int _catalogNumber;
+
+        internal StorageEntry(int catalogNumber)
+        {
+            _catalogNumber = catalogNumber;
+        }
     }
 }
