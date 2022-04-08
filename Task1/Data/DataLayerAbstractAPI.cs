c2d6bd44a91ca2506f2ba622d178476dd36ca9fc
Patryk <patryk.gielcz@gmail.com>
2022-04-08 09:24:38 +0200

    abstract data API according to TP repo

---
+++ Task1/Data/DataLayerAbstractAPI.cs
@@ -0,0 +1,24 @@
+﻿namespace Data
+{
+    public abstract class DataLayerAbstractAPI
+    {
+        public abstract void Connect();
+
+        public static DataLayerAbstractAPI CreateMyDataLayer()
+        {
+            return new MyDataLayer();
+        }
+
+        #region Layer implementation
+
+        private class MyDataLayer : DataLayerAbstractAPI
+        {
+            public override void Connect()
+            {
+                DataContext myData = new DataContext();
+            }
+
+            #endregion Layer implementation
+        }
+    }
+}
