50646df97a346b763ab25b62d0ac1c1a975cb43a
Patryk <patryk.gielcz@gmail.com>
2022-04-08 20:32:55 +0200

    DataAPI methods declared, not definded in abstraction yet

--- Task1/Data/DataLayerAbstractAPI.cs
+++ Task1/Data/DataLayerAbstractAPI.cs
@@ -2,31 +2,46 @@
 {
     public abstract class DataLayerAbstractAPI
     {
-        public abstract void Connect();
+        public abstract void InitializeDataContext();
 
         public static DataLayerAbstractAPI CreateMyDataLayer()
         {
             return new MyDataLayer();
         }
 
+        public abstract void addCustomer(Customer customer);
+        public abstract void addStorageEntry(StorageEntry storageEntry);
+        public abstract void addEvent(Event addedEvent);
+        public abstract void addCatalogEntry(int catalogNumber, Diamond diamond);
 
-
-
-
-
-
-        #region Layer implementation
 
         private class MyDataLayer : DataLayerAbstractAPI
         {
-            public override void Connect()
+            public override void addCatalogEntry(int catalogNumber, Diamond diamond)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void addCustomer(Customer customer)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void addEvent(Event addedEvent)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void addStorageEntry(StorageEntry storageEntry)
+            {
+                throw new NotImplementedException();
+            }
+
+            public override void InitializeDataContext()
             {
                 DataContext myData = new DataContext();
             }
 
-
-
-            #endregion Layer implementation
         }
     }
 }
