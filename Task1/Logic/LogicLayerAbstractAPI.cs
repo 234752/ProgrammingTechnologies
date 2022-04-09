c1ba826623302cb88a8563de2d1132cea6aa7151
Patryk <patryk.gielcz@gmail.com>
2022-04-09 14:30:42 +0200

    first functionality of Logic layer

--- Task1/Logic/LogicLayerAbstractAPI.cs
+++ Task1/Logic/LogicLayerAbstractAPI.cs
@@ -8,6 +8,7 @@ namespace Logic
         {
             return new MyLogicLayer(data == null ? DataLayerAbstractAPI.CreateMyDataLayer() : data);
         }
+        public abstract void addCustomer(int id, string name);
 
         private class MyLogicLayer : LogicLayerAbstractAPI
         {
@@ -15,9 +16,16 @@ namespace Logic
             {
                 _dataLayer = dataLayer;
                 _dataLayer.InitializeDataContext();
+                _service = new Service(dataLayer);
             }
             private readonly DataLayerAbstractAPI _dataLayer;
             //service class here, TODO
+            private Service _service;
+
+            public override void addCustomer(int id, string name)
+            {
+                _service.addCustomer(id, name);
+            }
         }
     }
 }
---
+++ Task1/Logic/Service.cs
@@ -0,0 +1,17 @@
+﻿using Data;
+
+namespace Logic
+{
+    internal class Service //whole logic should be in this class
+    {
+        private DataLayerAbstractAPI _dataLayer;
+        internal Service(DataLayerAbstractAPI dataLayer)
+        {
+            _dataLayer = dataLayer;
+        }
+        internal void addCustomer(int id, string name)
+        {
+            _dataLayer.addCustomer(id, name);
+        }
+    }
+}
