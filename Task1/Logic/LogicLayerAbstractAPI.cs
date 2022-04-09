bad966c20876bec217078ec38935453ebfaaa52f
Patryk <patryk.gielcz@gmail.com>
2022-04-09 13:57:27 +0200

    logic abstract API

---
+++ Task1/Logic/LogicLayerAbstractAPI.cs
@@ -0,0 +1,23 @@
+﻿using Data;
+
+namespace Logic
+{
+    public abstract class LogicLayerAbstractAPI
+    {
+        public static LogicLayerAbstractAPI CreateMyLogicLayer(DataLayerAbstractAPI data = default(DataLayerAbstractAPI)) //will it work as DI?
+        {
+            return new MyLogicLayer(data == null ? DataLayerAbstractAPI.CreateMyDataLayer() : data);
+        }
+
+        private class MyLogicLayer : LogicLayerAbstractAPI
+        {
+            public MyLogicLayer(DataLayerAbstractAPI dataLayer)
+            {
+                _dataLayer = dataLayer;
+                _dataLayer.InitializeDataContext();
+            }
+            private readonly DataLayerAbstractAPI _dataLayer;
+            //service class here, TODO
+        }
+    }
+}
