81c0dbefb935940fdda55a619acfe26e1e555ad0
olabanasiak <234750@edu.p.lodz.pl>
2022-04-11 01:26:33 +0200

    DataApi get Diamonds

--- Task1/Data/DataLayerAbstractAPI.cs
+++ Task1/Data/DataLayerAbstractAPI.cs
@@ -30,7 +30,7 @@ namespace Data
         //CATALOG
         public abstract void AddCatalogEntry(int catalogNumber, float carat, float price, int quality, int shape);
         public abstract bool RemoveCatalogEntry(int catalogEntryIndex);
-        public abstract void GetDiamondPrice(int catalogNumber);
+        public abstract string GetDiamonds(int catalogNumber);
 
         private class MyDataLayer : DataLayerAbstractAPI
         {
@@ -108,9 +108,9 @@ namespace Data
             {
                 return DataContext.StorageState.Remove(DataContext.StorageState.ElementAt(catalogEntryIndex));
             }
-            public override void GetDiamondPrice(int catalogNumber)
+            public override string GetDiamonds(int catalogNumber)
             {
-               
+               return DataContext.GetDiamond(catalogNumber);
             }
         }
     }
