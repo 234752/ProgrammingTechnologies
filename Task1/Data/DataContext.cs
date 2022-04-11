c82df589991de0d40d46139d9af93b7aef198922
Patryk <patryk.gielcz@gmail.com>
2022-04-11 18:41:46 +0200

    added 3rd data generation method

--- Task1/Data/DataContext.cs
+++ Task1/Data/DataContext.cs
@@ -13,7 +13,7 @@
             StorageState = new List<StorageEntry>();
             Events = new List<Event>();
         }
-        internal void InitializeCatalog() //possible DI, with empty or full initializer
+        internal void InitializeCatalog() 
         {
             Catalog = new Dictionary<int, Diamond>();
             Diamond first = new Diamond(1F, 2999.99F, QualityValue.Good, ShapeValue.Oval);
@@ -32,14 +32,27 @@
             Catalog.Add(6, seventh);
         }
 
-        internal void InitializeEmptyCatalog() //possible DI, with empty or full initializer
+        internal void InitializeEmptyCatalog() {}
+
+        internal void InitializeCatalogAndCustomerList() 
         {
-            Catalog = new Dictionary<int, Diamond>();
+            Diamond first = new Diamond(1F, 2999.99F, QualityValue.Good, ShapeValue.Oval);
+            Diamond second = new Diamond(1F, 3999.99F, QualityValue.Good, ShapeValue.Round);
+            Diamond third = new Diamond(2F, 4999.99F, QualityValue.VeryGood, ShapeValue.Pear);
+            Diamond fourth = new Diamond(2F, 6999.99F, QualityValue.Excellent, ShapeValue.Cushion);
+            Diamond fifth = new Diamond(3F, 1999.99F, QualityValue.Good, ShapeValue.Oval);
+            Diamond sixth = new Diamond(3F, 5999.99F, QualityValue.VeryGood, ShapeValue.Pear);
+            Diamond seventh = new Diamond(4F, 7999.99F, QualityValue.Excellent, ShapeValue.Cushion);
+            Catalog.Add(0, first);
+            Catalog.Add(1, second);
+            Catalog.Add(2, third);
+            Catalog.Add(3, fourth);
+            Catalog.Add(4, fifth);
+            Catalog.Add(5, sixth);
+            Catalog.Add(6, seventh);
+            Customers.Add(new Customer(0, "Bob"));
+            Customers.Add(new Customer(1, "Paul"));
+            Customers.Add(new Customer(2, "Daniel"));
         }
-
-        // returns value if exists
-        //public string GetDiamond (int key) => Catalog[key].ToString();
-
-
     }
 }
