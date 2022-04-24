69034b28f766ce93dd7cd0138e3c881b02636b3a
Patryk <patryk.gielcz@gmail.com>
2022-04-24 13:16:06 +0200

    integration tests fixed

--- Task1/UnitTests/IntegrationTests.cs
+++ Task1/UnitTests/IntegrationTests.cs
@@ -13,12 +13,14 @@ namespace UnitTests
             DataLayerAbstractAPI testedDataLayer = DataLayerAbstractAPI.CreateMyDataLayer();
             testedDataLayer.InitializeCatalog();
             LogicLayerAbstractAPI testedLogicLayer = LogicLayerAbstractAPI.CreateMyLogicLayer(testedDataLayer);
+            testedLogicLayer.AddCatalogEntry(1, 1F, 1999.99F, 2, 2);
+            testedLogicLayer.AddCatalogEntry(3, 1F, 3999.99F, 1, 1);
             testedLogicLayer.RegisterDelivery("12/12/2020", 1, 3);
             testedLogicLayer.RegisterDelivery("13/12/2020", 3, 2);
             Assert.AreEqual(testedDataLayer.GetAmountOfAllItems(), 5);
             Assert.AreEqual(testedDataLayer.GetAmountOfCatalogItem(1), 3);
             Assert.AreEqual(testedDataLayer.GetAmountOfCatalogItem(3), 2);
-            Assert.IsTrue(testedLogicLayer.AddCustomer(1, "BOB"));
+            Assert.IsTrue(testedLogicLayer.AddCustomer(0, "BOB"));
             testedLogicLayer.RegisterSale("14/12/2020", 3, 0);
             Assert.AreEqual(testedDataLayer.GetAmountOfAllItems(), 4);
             Assert.AreEqual(testedDataLayer.GetAmountOfCatalogItem(1), 3);
@@ -27,19 +29,23 @@ namespace UnitTests
         [TestMethod]
         public void TestRevenue()
         {
-            DataLayerAbstractAPI testedDataLayer = DataLayerAbstractAPI.CreateMyDataLayer();
-            testedDataLayer.InitializeCatalog();
-            LogicLayerAbstractAPI testedLogicLayer = LogicLayerAbstractAPI.CreateMyLogicLayer(testedDataLayer);
+            //DataLayerAbstractAPI testedDataLayer = DataLayerAbstractAPI.CreateMyDataLayer();
+            //testedDataLayer.InitializeCatalog();
+            LogicLayerAbstractAPI testedLogicLayer = LogicLayerAbstractAPI.CreateMyLogicLayer();
+            testedLogicLayer.AddCatalogEntry(0, 1F, 1999.99F, 2, 2);
+            testedLogicLayer.AddCatalogEntry(1, 1F, 3999.99F, 1, 1);
             testedLogicLayer.RegisterDelivery("12/12/2020", 0, 3);
+            testedLogicLayer.RegisterDelivery("14/12/2020", 1, 1);
 
             testedLogicLayer.AddCustomer(0, "BOB");
-            testedLogicLayer.RegisterSale("12/12/2020", 0, 0);
-            testedLogicLayer.RegisterSale("13/12/2020", 0, 0);
-            testedLogicLayer.RegisterSale("14/12/2020", 0, 0);
-            Assert.AreEqual(testedLogicLayer.CountRevenueFromSales(), 2999.99F * 3);
-            testedLogicLayer.RegisterDelivery("14/12/2020", 1, 1);
-            testedLogicLayer.RegisterSale("15/12/2020", 1, 0);
-            Assert.AreEqual(testedLogicLayer.CountRevenueFromSales(),2999.99F * 3 + 3999.99F);
+            testedLogicLayer.AddCustomer(1, "BOB1");
+            testedLogicLayer.AddCustomer(2, "BOB2");
+            Assert.IsTrue(testedLogicLayer.RegisterSale("12/12/2020", 0, 0));
+            Assert.IsTrue(testedLogicLayer.RegisterSale("13/12/2020", 0, 1));
+            Assert.IsTrue(testedLogicLayer.RegisterSale("14/12/2020", 0, 2));
+            Assert.AreEqual(testedLogicLayer.CountRevenueFromSales(), 1999.99F * 3);
+            testedLogicLayer.RegisterSale("15/12/2020", 1, 2);
+            Assert.AreEqual(testedLogicLayer.CountRevenueFromSales(), 1999.99F * 3 + 3999.99F);
         }
     }
 }
