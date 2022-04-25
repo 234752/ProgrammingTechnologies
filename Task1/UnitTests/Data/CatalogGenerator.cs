284c5cb817b7695829c779b3f251265dae5caf1b
Patryk <patryk.gielcz@gmail.com>
2022-04-25 19:31:59 +0200

    Revert "Update CatalogGenerator.cs"
    This reverts commit e5e1f9cbe7c087a91ab8b49299dd05ab34128507.

--- Task1/UnitTests/Data/CatalogGenerator.cs
+++ Task1/UnitTests/Data/CatalogGenerator.cs
@@ -9,8 +9,8 @@ namespace UnitTests.Data
             dataLayer.AddCatalogEntry(0, 1F, 2999.99F, 1, 1);
             dataLayer.AddCatalogEntry(1, 1F, 3999.99F, 2, 2);
             dataLayer.AddCatalogEntry(2, 2F, 4999.99F, 3, 3);
-            dataLayer.AddCatalogEntry(3, 2F, 6999.99F, 4, 4);
-            dataLayer.AddCatalogEntry(4, 3F, 1999.99F, 5, 5);
+            dataLayer.AddCatalogEntry(3, 2F, 6999.99F, 3, 4);
+            dataLayer.AddCatalogEntry(4, 3F, 1999.99F, 4, 4);
         }
     }
 }
