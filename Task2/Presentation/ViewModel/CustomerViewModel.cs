04ea650419ebdae0111c356f3fdc9704ea99ac1c
Patryk <patryk.gielcz@gmail.com>
2022-05-17 22:34:40 +0200

    viewModel connected with view, 'something is shown'

---
+++ Task2/Presentation/ViewModel/CustomerViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Models;
+
+namespace Presentation.ViewModel
+{
+    internal class CustomerViewModel
+    {
+        
+        private DataModel model = new DataModel();
+        private int _Id = 7;
+        public CustomerViewModel()
+        {
+            Id = model.Customers.ElementAt(1).Id;
+        }
+
+        public int Id 
+        { 
+            get 
+            { 
+                return _Id; 
+            }
+            set
+            {
+                _Id = value;
+            }
+        }
+    }
+}
