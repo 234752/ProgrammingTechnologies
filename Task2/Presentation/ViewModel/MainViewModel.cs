d7715c75d2caa382c79b236e4d384fccbf59b7f8
Patryk <patryk.gielcz@gmail.com>
2022-05-18 12:56:57 +0200

    added customer list to main view

--- Task2/Presentation/ViewModel/MainViewModel.cs
+++ Task2/Presentation/ViewModel/MainViewModel.cs
@@ -10,10 +10,13 @@ namespace Presentation.ViewModel
     internal class MainViewModel
     {
         private CustomerView _InnerView;
+        private CustomerListView _InnerView2;
         public MainViewModel()
         {
             _InnerView = new CustomerView();
+            _InnerView2 = new CustomerListView();
         }
         public CustomerView InnerView { get { return _InnerView; } set { _InnerView = value; } }
+        public CustomerListView InnerView2 { get { return _InnerView2; } set { _InnerView2 = value; } }
     }
 }
