31a8c34c58c81eaaf3acfc748400e8f8367691a1
Patryk <patryk.gielcz@gmail.com>
2022-05-18 15:21:30 +0200

    list view displays sample arrray

---
+++ Task2/Presentation/ViewModel/CustomerListViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Models;
+
+namespace Presentation.ViewModel
+{
+    internal class CustomerListViewModel
+    {
+        private DataModel model = new DataModel();
+        private List<String> _Customers = new List<string>() { "customer1", "customer2" };
+
+        public CustomerListViewModel()
+        {
+           
+        }
+        public List<String> Customers
+        {
+            get
+            {
+                return _Customers;
+            }
+            set
+            {
+                _Customers = value;
+            }
+        }
+    }
+}
