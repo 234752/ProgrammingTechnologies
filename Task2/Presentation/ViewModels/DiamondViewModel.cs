86412c23e0d9b4e628debf02232383b83f277f99
Patryk <patryk.gielcz@gmail.com>
2022-05-19 14:45:43 +0200

    Diamond view model

---
+++ Task2/Presentation/ViewModels/DiamondViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Models;
+using Presentation.ViewModels.MVVMLight;
+
+namespace Presentation.ViewModels
+{
+    internal class DiamondViewModel : BaseViewModel
+    {
+        private int _Id;
+        private string _Name;
+        private string _Quality;
+        private decimal _Price;
+        public DiamondViewModel(int id, string name, string quality, decimal price)
+        {
+            _Id = id;
+            _Name = name;
+            _Quality = quality;
+            _Price = price;
+        }
+
+        public int Id { get { return _Id; } set { _Id = value; } }
+        public string Name { get { return _Name; } set { _Name = value; } }
+        public string Quality { get { return _Quality; } set { _Quality = value; } }
+        public decimal Price { get { return _Price; } set { _Price = value; } }
+        
+    }
+}
