7fd9ee4074f932aec15b244996bffd02737dd3ab
Patryk <patryk.gielcz@gmail.com>
2022-05-19 15:26:03 +0200

    views can be changed with buttons

--- Task2/Presentation/ViewModels/MainViewModel.cs
+++ Task2/Presentation/ViewModels/MainViewModel.cs
@@ -13,16 +13,30 @@ namespace Presentation.ViewModels
     public class MainViewModel : BaseViewModel
     {
         private UserControl _InnerView;
-        private ICommand _ChangeInnerView;
+        private ICommand _ChangeInnerViewToCustomers;
+        private ICommand _ChangeInnerViewToDiamonds;
+        private ICommand _ChangeInnerViewToEvents;
         public MainViewModel()
         {
-            _ChangeInnerView = new RelayCommand(() => ChangeView());
+            _ChangeInnerViewToCustomers = new RelayCommand(() => ChangeViewToCustomers());
+            _ChangeInnerViewToDiamonds = new RelayCommand(() => ChangeViewToDiamonds());
+            _ChangeInnerViewToEvents = new RelayCommand(() => ChangeViewToEvents());
         }
         public UserControl InnerView { get { return _InnerView; } set { _InnerView = value; RaisePropertyChanged(nameof(InnerView)); } }
-        public ICommand ChangeInnerView { get { return _ChangeInnerView; } }
-        public void ChangeView()
+        public ICommand ChangeInnerViewToCustomers { get { return _ChangeInnerViewToCustomers; } }
+        public ICommand ChangeInnerViewToDiamonds { get { return _ChangeInnerViewToDiamonds; } }
+        public ICommand ChangeInnerViewToEvents { get { return _ChangeInnerViewToEvents; } }
+        public void ChangeViewToCustomers()
+        {
+            InnerView = new CustomerListView();
+        }
+        public void ChangeViewToDiamonds()
         {
             InnerView = new DiamondListView();
+        }
+        public void ChangeViewToEvents()
+        {
+            //InnerView = new EventListView();
         }
     }
 }
