f3faac7583a9dde5caea54ffd10c38ea5deac240
Patryk <patryk.gielcz@gmail.com>
2022-05-19 15:43:40 +0200

    event list view model

---
+++ Task2/Presentation/ViewModels/EventListViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Models;
+using Presentation.ViewModels.MVVMLight;
+
+namespace Presentation.ViewModels
+{
+    public class EventListViewModel : BaseViewModel
+    {
+        private DataModel model = new DataModel();
+        private ObservableCollection<EventViewModel> _Events = new ObservableCollection<EventViewModel>();
+        private EventViewModel _CurrentEvent;
+
+        public EventListViewModel()
+        {
+            _Events = new ObservableCollection<EventViewModel>();
+            foreach (EventModel ev in model.Events)
+            {
+                _Events.Add(new EventViewModel(ev.Id, ev.Date, ev.IsDelivery, ev.CatalogId));
+            }
+        }
+        public ObservableCollection<EventViewModel> Events
+        {
+            get
+            {
+                return _Events;
+            }
+            set
+            {
+                _Events = value;
+            }
+        }
+        public EventViewModel CurrentEvent
+        {
+            get
+            {
+                return _CurrentEvent;
+            }
+            set
+            {
+                _CurrentEvent = value;
+                RaisePropertyChanged(nameof(CurrentEvent));
+            }
+        }
+    }
+}
