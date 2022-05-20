76bea4b6951adf0cdf3a5a41f1f5a059f776aec9
Patryk <patryk.gielcz@gmail.com>
2022-05-20 19:40:40 +0200

    mocked classes addet to tests project

---
+++ Task2/TestPresentation/MockedModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Presentation.Models.ModelsAPI;
+
+namespace TestPresentation
+{
+    public class MockCustomer : ICustomerModel
+    {
+        public int Id { get { return 102; } set { Id = value; } }
+        public string FirstName { get { return "FirstName"; } set { FirstName = value; } }
+        public string LastName { get { return "LastName"; } set { LastName = value; } }
+        public override string ToString()
+        {
+            return "Customer Mock";
+        }
+    }
+
+    public class MockDiamond : IDiamondModel
+    {
+        public int Id { get { return 102; } set { Id = value; } }
+        public string Name { get { return "Name"; } set { Name = value; } }
+        public string Quality { get { return "Quality"; } set { Quality = value; } }
+        public decimal Price { get { return 10.2M; } set { Price = value; } }
+
+        public override string ToString()
+        {
+            return "Diamond Mock";
+        }
+    }
+
+    public class MockEvent : IEventModel
+    {
+        public int Id { get { return 102; } set { Id = value; } }
+        public string Date { get { return "01/01/2000"; } set { Date = value; } }
+        public bool IsDelivery { get { return true; } set { IsDelivery = value; } }
+        public int CatalogId { get { return 102; } set { CatalogId = value; } }
+        public override string ToString()
+        {
+            return "Event Mock";
+        }
+    }
+
+    public class MockDataModel : IDataModel
+    {
+        public IEnumerable<ICustomerModel> Customers
+        {
+            get
+            {
+                return Customers;
+            }
+        }
+
+        public IEnumerable<IDiamondModel> Diamonds
+        {
+            get
+            {
+                return Diamonds;
+            }
+        }
+
+        public IEnumerable<IEventModel> Events
+        {
+            get
+            {
+                return Events;
+            }
+        }
+    }
+}
+}
---
+++ Task2/TestPresentation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestPresentation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestPresentation")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d1f66e71-ef7c-48bb-8780-66b673817d24")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
