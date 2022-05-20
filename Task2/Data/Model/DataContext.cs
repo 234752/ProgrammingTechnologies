a5b5cbc9782e3d26c97d7ec672a282ca5591e235
olabanasiak <234750@edu.p.lodz.pl>
2022-05-20 22:33:17 +0200

    StorageEntry deleted in the project

--- Task2/Data/Model/DataContext.cs
+++ Task2/Data/Model/DataContext.cs
@@ -5,14 +5,12 @@ namespace Data
     internal class DataContext
     {
         internal List<Customer> Customers;
-        internal List<StorageEntry> StorageState; //maybe its useless?
         internal List<Event> Events;    
         internal List<Diamond> Catalog;  
 
         internal DataContext()
         {
             Customers = new List<Customer>();
-            StorageState = new List<StorageEntry>();
             Events = new List<Event>();
             Catalog = new List<Diamond>();
         }
