663bd4d51c341717cb9e38b0f75f4c5f04d5fed5
Patryk <patryk.gielcz@gmail.com>
2022-05-21 16:53:56 +0200

    add event

--- Task2/Presentation/ViewModels/EventListViewModel.cs
+++ Task2/Presentation/ViewModels/EventListViewModel.cs
@@ -17,15 +17,19 @@ namespace Presentation.ViewModels
         private ObservableCollection<EventViewModel> _Events = new ObservableCollection<EventViewModel>();
         private EventViewModel _CurrentEvent;
         private ICommand _RemoveEventCommand;
+        private ICommand _AddEventCommand;
+        private int _NextEventId = 0;
 
         public EventListViewModel()
         {
             _Events = new ObservableCollection<EventViewModel>();
             foreach (IEventModel ev in model.Events)
             {
-                _Events.Add(new EventViewModel(ev.Id, ev.Date, ev.IsDelivery, ev.CatalogId));
+                _Events.Add(new EventViewModel(_NextEventId, ev.Date, ev.IsDelivery, ev.CatalogId));
+                _NextEventId++;
             }
             _RemoveEventCommand = new RelayCommand(() => RemoveEvent());
+            _AddEventCommand = new RelayCommand(() => AddEvent());
         }
         public EventListViewModel(IDataModel dataModel)
         {
@@ -36,15 +40,23 @@ namespace Presentation.ViewModels
                 _Events.Add(new EventViewModel(ev.Id, ev.Date, ev.IsDelivery, ev.CatalogId));
             }
             _RemoveEventCommand = new RelayCommand(() => RemoveEvent());
+            _AddEventCommand = new RelayCommand(() => AddEvent());
+
         }
         public ObservableCollection<EventViewModel> Events
         { get { return _Events; } set { _Events = value; RaisePropertyChanged(nameof(Events)); } }
         public EventViewModel CurrentEvent
         { get { return _CurrentEvent; } set { _CurrentEvent = value; RaisePropertyChanged(nameof(CurrentEvent)); } }
         public ICommand RemoveEventCommand { get { return _RemoveEventCommand; } }
+        public ICommand AddEventCommand { get { return _AddEventCommand; } }
         public void RemoveEvent()
         {
             Events.Remove(CurrentEvent);
+        }
+        public void AddEvent()
+        {
+            Events.Add(new EventViewModel() { Id = _NextEventId, CatalogId = 0, Date = "", IsDelivery = true});
+            _NextEventId++;
         }
     }
 }
