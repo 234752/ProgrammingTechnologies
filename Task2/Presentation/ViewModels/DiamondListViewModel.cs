2bdf275daf23de501b36a8aa79cbace305970171
Patryk <patryk.gielcz@gmail.com>
2022-05-21 14:45:17 +0200

    add diamond

--- Task2/Presentation/ViewModels/DiamondListViewModel.cs
+++ Task2/Presentation/ViewModels/DiamondListViewModel.cs
@@ -17,15 +17,19 @@ namespace Presentation.ViewModels
         private ObservableCollection<DiamondViewModel> _Diamonds = new ObservableCollection<DiamondViewModel>();
         private DiamondViewModel _CurrentDiamond;
         private ICommand _RemoveDiamondCommand;
+        private ICommand _AddDiamondCommand;
+        private int _NextDiamondId = 0;
 
         public DiamondListViewModel()
         {
             _Diamonds = new ObservableCollection<DiamondViewModel>();
             foreach (IDiamondModel diamond in model.Diamonds)
             {
-                _Diamonds.Add(new DiamondViewModel(diamond.Id, diamond.Name, diamond.Quality, diamond.Price));
+                _Diamonds.Add(new DiamondViewModel(_NextDiamondId, diamond.Name, diamond.Quality, diamond.Price));
+                _NextDiamondId++;
             }
             _RemoveDiamondCommand = new RelayCommand(() => RemoveDiamond());
+            _AddDiamondCommand = new RelayCommand(() => AddDiamond());
         }
         public DiamondListViewModel(IDataModel dataModel)
         {
@@ -36,16 +40,23 @@ namespace Presentation.ViewModels
                 _Diamonds.Add(new DiamondViewModel(diamond.Id, diamond.Name, diamond.Quality, diamond.Price));
             }
             _RemoveDiamondCommand = new RelayCommand(() => RemoveDiamond());
+            _AddDiamondCommand = new RelayCommand(() => AddDiamond());
         }
         public ObservableCollection<DiamondViewModel> Diamonds
         { get { return _Diamonds; } set { _Diamonds = value; RaisePropertyChanged(nameof(Diamonds)); } }
         public DiamondViewModel CurrentDiamond
         { get { return _CurrentDiamond; } set { _CurrentDiamond = value; RaisePropertyChanged(nameof(CurrentDiamond)); } }
         public ICommand RemoveDiamondCommand { get { return _RemoveDiamondCommand; } }
+        public ICommand AddDiamondCommand { get { return _AddDiamondCommand; } }
 
         public void RemoveDiamond()
         {
             Diamonds.Remove(CurrentDiamond);
+        }
+        public void AddDiamond()
+        {
+            Diamonds.Add(new DiamondViewModel() { Id = _NextDiamondId, Name = "", Price = 0M, Quality = ""});
+            _NextDiamondId++;
         }
     }
 }
