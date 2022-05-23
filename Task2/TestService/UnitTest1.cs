7490dd8f08eca0b9874504b0eb2da48b92e05316
Patryk <patryk.gielcz@gmail.com>
2022-05-23 21:50:20 +0200

    more test of service

--- Task2/TestService/UnitTest1.cs
+++ Task2/TestService/UnitTest1.cs
@@ -22,18 +22,23 @@ namespace TestProject2
             Assert.IsTrue(custService.AddCustomer(1, "Piotr", "Makin"));
             dataLayer.AddCustomer(1, "Piotr", "Makin").Returns(false);
             Assert.IsFalse(custService.AddCustomer(1, "Piotr", "Makin"));
-
         }
         public void TestDiamonds()
         {
             DiamondService diamService = new DiamondService(dataLayer);
-           
-
+            dataLayer.AddDiamond(1, 13.37M, "Highest Quality", "Very Big Diamond").Returns(true);
+            Assert.IsTrue(diamService.AddDiamond(1, 13.37M, "Highest Quality", "Very Big Diamond"));
+            dataLayer.AddDiamond(1, 13.37M, "Highest Quality", "Very Big Diamond").Returns(false);
+            Assert.IsFalse(diamService.AddDiamond(1, 13.37M, "Highest Quality", "Very Big Diamond"));
         }
         public void TestEvents()
         {
             EventService evService = new EventService(dataLayer);
-            
+            dataLayer.AddEvent(3, "20/05/2022", "true", 1,1).Returns(true);
+            Assert.IsTrue(evService.AddEvent(3, "20/05/2022", "true", 1, 1));
+            dataLayer.AddEvent(3, "20/05/2022", "true", 1, 1).Returns(false);
+            Assert.IsFalse(evService.AddEvent(3, "20/05/2022", "true", 1, 1));
+
         }
     }
 }
