2d8616a4626ed18807c5730ddad7bd98e41b1beb
Patryk <patryk.gielcz@gmail.com>
2022-05-23 19:13:29 +0200

    2nd and last test of data layer

--- Task2/TestData/TestData.cs
+++ Task2/TestData/TestData.cs
@@ -26,7 +26,19 @@ namespace TestData
             dataLayer.AddEvent(0, "20/05/2022", "True", 0, 0);
             Assert.AreEqual(dataLayer.GetEvent(0).Date, "20/05/2022");
         }
-        
+
+        [TestMethod]
+        public void TestRemove()
+        {
+            IDataAPI dataLayer = IDataAPI.CreateLayer("Data Source=LAPTOP-6CIA6OPN;Initial Catalog=DiamondShop;Integrated Security=True");
+            dataLayer.AddCustomer(0, "bob", "surname");
+            Assert.AreEqual(dataLayer.GetCustomer(0).Name, "bob");
+
+            dataLayer.DeleteCustomer(0);
+            Assert.AreEqual(dataLayer.GetCustomer(0), null);
+
+        }
+
 
     }
 }
