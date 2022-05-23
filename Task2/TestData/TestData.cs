fca5735d79f8b60525056b9b3a1e0e0776e40926
Patryk <patryk.gielcz@gmail.com>
2022-05-23 19:07:02 +0200

    data layer tests, failed due to no connection

--- Task2/TestData/TestData.cs
+++ Task2/TestData/TestData.cs
@@ -6,9 +6,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-
-
 using Data.API;
 
 namespace TestData
@@ -17,9 +14,17 @@ namespace TestData
     public class RepositoryTest
     {
         [TestMethod]
-        public void test1()
+        public void TestConnection()
         {
-            IDataAPI dataLayer = IDataAPI.CreateLayer("DiamondShop");
+            IDataAPI dataLayer = IDataAPI.CreateLayer("Data Source=LAPTOP-6CIA6OPN;Initial Catalog=DiamondShop;Integrated Security=True");
+            dataLayer.AddCustomer(0, "bob", "surname");
+            Assert.AreEqual(dataLayer.GetCustomer(0).Name, "bob");
+
+            dataLayer.AddDiamond(0, 102.20M, "Very Good");
+            Assert.AreEqual(dataLayer.GetDiamond(0).Price, 201.2M);
+
+            dataLayer.AddEvent(0, "20/05/2022", "True", 0, 0);
+            Assert.AreEqual(dataLayer.GetEvent(0).Date, "20/05/2022");
         }
         
 
